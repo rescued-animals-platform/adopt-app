290eae4b6ac4cec93a7908a4516da6874b9be891
Luisa Emme <emmeblm@users.noreply.github.com>
2022-06-11 20:07:03 +0200

    [Luisa][chore] Initial project setup fixing android test issues

--- shared/src/commonMain/kotlin/com/example/adoptapp/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.example.adoptapp
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/example/adoptapp/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adoptapp
-
-expect class Platform() {
-    val platform: String
-}
